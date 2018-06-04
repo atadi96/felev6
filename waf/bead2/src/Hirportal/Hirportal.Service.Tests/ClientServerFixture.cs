792e5a3a27bc79fe95fd469409729e54c1b3f0cf
Abonyi-Tóth Ádám <atadi96@gmail.com>
2018-06-04 07:13:55 +0200

    A failed attempt at asp.net core unit testing

---
+++ waf/bead2/src/Hirportal/Hirportal.Service.Tests/ClientServerFixture.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Hirportal.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
+using System.Reflection;
+
+namespace Hirportal.Service.Tests
+{
+    public class DatabaseSeeder
+    {
+        private readonly UserManager<Author> _userManager;
+        private readonly NewsContext _context;
+
+        public DatabaseSeeder(NewsContext context, UserManager<Author> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task Seed()
+        {
+            Author testAuthor = new Author() { UserName = "test", Name = "test" };
+            var result = _userManager.CreateAsync(testAuthor, "test");
+
+            Article testArticle = new Article()
+            {
+                Title = "Title",
+                Author = testAuthor,
+                Content = "Content",
+                Description = "Description",
+                Leading = false
+            };
+            _context.Authors.Add(testAuthor);
+            _context.Articles.Add(testArticle);
+
+            _context.SaveChanges();
+        }
+    }
+
+    public class TestStartup
+    {
+        public TestStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<NewsContext>(options =>
+                options.UseInMemoryDatabase("HirtportalTestDb")
+            );
+
+            services.AddIdentity<Author, IdentityRole<int>>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequiredUniqueChars = 0;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+            })
+            .AddEntityFrameworkStores<NewsContext>()
+            .AddDefaultTokenProviders();
+
+            services.AddTransient<DatabaseSeeder>();
+
+            services.AddMvc().AddApplicationPart(Assembly.Load(new AssemblyName("Hirportal.Service")));
+        }
+
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // Now seed the database
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var seeder = serviceScope.ServiceProvider.GetService<DatabaseSeeder>();
+                await seeder.Seed();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+
+    public class ClientServerFixture : IDisposable
+    {
+        public TestServer TestServer { get; private set; }
+        public HttpClient Client { get; private set; }
+
+        public ClientServerFixture()
+        {
+            var integrationTestsPath = PlatformServices.Default.Application.ApplicationBasePath;
+            var applicationPath = Path.GetFullPath(Path.Combine(integrationTestsPath, "../../../../Hirportal.Service"));
+
+            TestServer = new TestServer(WebHost.CreateDefaultBuilder()
+                .UseStartup<TestStartup>()
+                .UseEnvironment("Development"));
+            Client = TestServer.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            TestServer?.Dispose();
+            Client?.Dispose();
+        }
+    }
+}
---
+++ waf/bead2/src/Hirportal/Hirportal.Service.Tests/HirportalTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Xunit;
+using Hirportal.Service.Controllers;
+using Hirportal.Persistence;
+using Hirportal.Persistence.DTO;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Hirportal.Service.Tests
+{
+    public class HirportalTest : IClassFixture<ClientServerFixture>
+    {
+        ClientServerFixture fixture;
+
+        public HirportalTest(ClientServerFixture fxt)
+        {
+            fixture = fxt;
+        }
+
+        [Fact]
+        public async Task CantSeeArticlesWithoutLoggingIn()
+        {
+            var response = await fixture.Client.GetAsync("api/articles/");
+            Assert.Equal(System.Net.HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task TestCanLogin()
+        {
+            var response = await fixture.Client.GetAsync("api/account/login/test/test");
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task LoggedInCanSeeArticles()
+        {
+            var response = await fixture.Client.GetAsync("api/articles/");
+
+            response.EnsureSuccessStatusCode();
+            Assert.Single(await response.Content.ReadAsAsync<IEnumerable<ArticlePreviewDTO>>());
+        }
+
+        [Fact]
+        public async Task CanPostValidArticle()
+        {
+            var response = await fixture.Client.PostAsJsonAsync<ArticleUploadDTO>("api/articles/", new ArticleUploadDTO()
+            {
+                Content = "Valid content",
+                DeleteImages = false,
+                Description = "Valid description",
+                Leading = false,
+                NewImages = new byte[0][],
+                Title = "Valid title"
+            });
+
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task CantPostInvalidArticle()
+        {
+            var response = await fixture.Client.PostAsJsonAsync<ArticleUploadDTO>("api/articles/", new ArticleUploadDTO()
+            {
+                Content = "Valid content",
+                DeleteImages = false,
+                Description = "Valid description",
+                Leading = true,
+                NewImages = new byte[0][],
+                Title = "Valid title"
+            });
+
+            Assert.False(response.IsSuccessStatusCode);
+        }
+    }
+}
